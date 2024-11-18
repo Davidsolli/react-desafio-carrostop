4119d0cb06e21f79d43591947fdb6233a99cc826
Davidsolli <davidolisoa@gmail.com>
2024-11-18 20:19:05 -0300

    first commit: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <>
+      <header className="dsc-header">
+        <nav className="dsc-container dsc-nav">
+          <h1>DSCarrosTop</h1>
+        </nav>
+      </header>
+    </>
+  );
+}
