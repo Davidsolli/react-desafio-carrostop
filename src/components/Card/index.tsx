fc6faf1896c64ab46b0e34c6e6cf9f2cd52d56d6
Davidsolli <davidolisoa@gmail.com>
2024-11-19 22:15:14 -0300

    card component and catalog section

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import carImg from "../../assets/car-card.png";
+
+export default function Card() {
+  return (
+    <>
+      <div className="car-card dsc-catalog-container">
+        <img src={carImg} alt="carImg" />
+        <h4>Lorem ipsum dolor</h4>
+      </div>
+    </>
+  );
+}
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,3 +1,4 @@
+import Card from "../../components/Card";
 import Header from "../../components/Header";
 import "./styles.css";
 
@@ -5,6 +6,22 @@ export default function Home() {
   return (
     <>
       <Header />
+
+      <section id="dsc-catalog-container">
+        <h2>Venha nos visitar</h2>
+        <div className="dsc-cards-area">
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+        </div>
+      </section>
+      
+      <section className="dsc-coments">
+
+      </section>
     </>
   );
 }
