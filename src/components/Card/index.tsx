35cf2a982035316633e876a247e0607c2fbd753b
Davidsolli <davidolisoa@gmail.com>
2024-11-20 14:11:13 -0300

    comment component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,7 +4,7 @@ import carImg from "../../assets/car-card.png";
 export default function Card() {
   return (
     <>
-      <div className="car-card dsc-catalog-container">
+      <div className="dsc-car-card dsc-catalog-container">
         <img src={carImg} alt="carImg" />
         <h4>Lorem ipsum dolor</h4>
       </div>
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,4 +1,5 @@
 import Card from "../../components/Card";
+import Comment from "../../components/Comment";
 import Header from "../../components/Header";
 import "./styles.css";
 
@@ -10,17 +11,39 @@ export default function Home() {
       <section id="dsc-catalog-container">
         <h2>Venha nos visitar</h2>
         <div className="dsc-cards-area">
-          <div className="card">
+          <div className="dsc-card">
             <Card />
           </div>
-          <div className="card">
+          <div className="dsc-card">
             <Card />
           </div>
         </div>
       </section>
-      
-      <section className="dsc-coments">
 
+      <section id="dsc-coments-container">
+        <div className="dsc-container">
+          <h2>O que estão dizendo</h2>
+          <div className="dsc-coments-area">
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+            <div className="dsc-comment">
+              <Comment />
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
